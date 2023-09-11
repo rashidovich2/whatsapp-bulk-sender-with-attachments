70c5f8188d3eed4b7b9e3dbe5936c68437801f0d
Sourcery AI <>
2023-09-11 14:22:16 +0000

    'Refactored by Sourcery'

--- automater.py.py
+++ automater.py.py
@@ -41,10 +41,8 @@ print("**********************************************************")
 print("**********************************************************")
 print(style.RESET)
 
-f = open("message.txt", "r")
-message = f.read()
-f.close()
-
+with open("message.txt", "r") as f:
+    message = f.read()
 print(style.YELLOW + '\nThis is your message-')
 print(style.GREEN + message)
 print("\n" + style.RESET)
@@ -52,13 +50,12 @@ message = quote(message)
 
 
 numbers = []
-f = open("numbers.txt", "r")
-for line in f.read().splitlines():
-	if line.strip() != "":
-		numbers.append(line.strip())
-f.close()
+with open("numbers.txt", "r") as f:
+    numbers.extend(
+        line.strip() for line in f.read().splitlines() if line.strip() != ""
+    )
 total_number=len(numbers)
-print(style.RED + 'We found ' + str(total_number) + ' numbers in the file' + style.RESET)
+print(f'{style.RED}We found {total_number} numbers in the file{style.RESET}')
 delay = 30
 filename=input("Enter the file name exactly: ")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
@@ -66,38 +63,40 @@ print('Once your browser opens up sign in to web whatsapp')
 driver.get('https://web.whatsapp.com')
 sleep(10)
 for idx, number in enumerate(numbers):
-	number = number.strip()
-	if number == "":
-		continue
-	print(style.YELLOW + '{}/{} => Sending message to {}.'.format((idx+1), total_number, number) + style.RESET)
-	try:
-		url = 'https://web.whatsapp.com/send?phone=' + number + '&text=' + message
-		sent = False
-		for i in range(3):
-			if not sent:
-				driver.get(url)
-				try:
-					click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='compose-btn-send']")))
-				except Exception as e:
-					print(style.RED + f"\nFailed to send message to: {number}, retry ({i+1}/3)")
-					print("Make sure your phone and computer is connected to the internet.")
-					print("If there is an alert, please dismiss it." + style.RESET)
-				else:
-					sleep(30)
-					click_btn.click()
-					sleep(2)
-					filen=(f"Give the path name here/{filename}")#path name where file is stored Eg: "C:/Users/Desktop/bot/
-					driver.find_element_by_css_selector('span[data-icon="clip"]').click()
-					attach=driver.find_element_by_css_selector('input[type="file"]')
-					attach.send_keys(filen)
-					send=driver.find_element_by_class_name('_2QbRL')
-					sleep(5)
-					go=driver.find_element_by_xpath("//*[@id='app']/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div/span")
-					go.click()
-					sleep(1)
-					sent=True
-					sleep(5)
-					result=print(style.GREEN + 'Message sent to: ' + number + style.RESET)
-	except Exception as e:
-		print(style.RED + 'Failed to send message to ' + number + str(e) + style.RESET)
+    number = number.strip()
+    if number == "":
+    	continue
+    print(
+        f'{style.YELLOW}{idx + 1}/{total_number} => Sending message to {number}.{style.RESET}'
+    )
+    try:
+        url = f'https://web.whatsapp.com/send?phone={number}&text={message}'
+        sent = False
+        for i in range(3):
+            if not sent:
+                driver.get(url)
+                try:
+                    click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='compose-btn-send']")))
+                except Exception as e:
+                    print(f"{style.RED}\nFailed to send message to: {number}, retry ({i + 1}/3)")
+                    print("Make sure your phone and computer is connected to the internet.")
+                    print(f"If there is an alert, please dismiss it.{style.RESET}")
+                else:
+                    sleep(30)
+                    click_btn.click()
+                    sleep(2)
+                    filen=(f"Give the path name here/{filename}")#path name where file is stored Eg: "C:/Users/Desktop/bot/
+                    driver.find_element_by_css_selector('span[data-icon="clip"]').click()
+                    attach=driver.find_element_by_css_selector('input[type="file"]')
+                    attach.send_keys(filen)
+                    send=driver.find_element_by_class_name('_2QbRL')
+                    sleep(5)
+                    go=driver.find_element_by_xpath("//*[@id='app']/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div/span")
+                    go.click()
+                    sleep(1)
+                    sent=True
+                    sleep(5)
+                    result = print(f'{style.GREEN}Message sent to: {number}{style.RESET}')
+    except Exception as e:
+        print(f'{style.RED}Failed to send message to {number}{str(e)}{style.RESET}')
 driver.close()
